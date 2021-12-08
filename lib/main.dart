9a683ffbb26d0b91fd4b9e412e54f8eb08c8de9f
Ayush783 <ayushc45xxx@gmail.com>
2021-12-08 22:02:20 +0530

    init

---
+++ lib/main.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        body: Center(
+          child: Container(
+            height: 100,
+            width: 100,
+            color: Colors.blue,
+          ),
+        ),
+      ),
+    );
+  }
+}
